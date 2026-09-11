9f2d34bbdd22181d92f45e905839d28149fc054d
AI-Manual <valhiroyuki@yahoo.co.jp>
2026-09-11 14:22:19 +0900

    Update index.js

--- worker/index.js
+++ worker/index.js
@@ -1,1 +1,19 @@
+export default {
 
+async fetch(request,env){
+
+ return new Response(
+ JSON.stringify({
+  success:true,
+  message:"AI Manual Worker OK"
+ }),
+ {
+  headers:{
+   "content-type":"application/json"
+  }
+ }
+ );
+
+}
+
+}
